77f341fe152bc03d41d4463a42235a4949de8f18
mts_dev3 <mts_dev3@stephensdirect.com>
2023-04-18 10:25:58 -0400

    added arrow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Fixedarrow from "./components/fixedArrow/Fixedarrow";
 import Fixedtop from "./components/fixedTop/Fixedtop";
 
 function App() {
   return (
     <div className="App">
       <Fixedtop />
+      <Fixedarrow />
     </div>
   );
 }
---
+++ src/components/fixedArrow/Fixedarrow.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './fixedArrow.css';
+
+export default function Fixedarrow() {
+  return (
+    <div className='fixedArrowWrapper'>
+      <div className="fixedArrowContent">
+        <div className="arrowButton">
+        <span class="material-symbols-outlined">
+        arrow_downward
+        </span>
+        </div>
+
+      </div>
+    </div>
+  )
+}
