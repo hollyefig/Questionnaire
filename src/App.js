15a106636b29a8a82391955f0e5e1994e4337f6d
mts_dev3 <mts_dev3@stephensdirect.com>
2023-04-18 08:55:04 -0400

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [word, setWord] = useState("Hello");
+
+  setTimeout(() => {
+    setWord("There");
+  }, 2000);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{word}</p>
       </header>
     </div>
   );
