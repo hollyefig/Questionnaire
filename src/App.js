b873ff9f9cd32ad95741f3d4c615a12942d3151e
mts_dev3 <mts_dev3@stephensdirect.com>
2023-04-18 09:05:04 -0400

    changed text

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [word, setWord] = useState("Hello");
+  const [word, setWord] = useState("Hello There");
 
   setTimeout(() => {
-    setWord("There");
+    setWord("General Kenobi");
   }, 2000);
   return (
     <div className="App">
