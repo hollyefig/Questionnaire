549b5998afe00b7961ba2dbb751b22949e1f2272
mts_dev3 <mts_dev3@stephensdirect.com>
2023-04-19 16:27:12 -0400

    created data for questions. input into q1

--- src/components/questions/Question1.jsx
+++ src/components/questions/Question1.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import './questions.css';
+import { questionData } from '../../questionData';
 
 export default function Question1({q1ref}) {
+
+console.log(questionData[0].answers)
 
 
   return (
     <div className='question1Wrapper' id="question1Wrapper" ref={q1ref}>
-      <div className="q1ContentWindow">content Window</div>
+      <div className="q1ContentWindow">
+        <div className="qHeading">
+          <h2>{questionData[0].question}</h2>
+        </div>
+        <div className="qAnswerWrapper">
+          <ul>
+
+            {questionData[0].answers.map(e => {
+              return (
+              <li>
+              <input type='radio' name={questionData[0].radioName} id={e.id}/>
+              <label for={e.id}>{e.answer}</label>
+              </li>
+              )
+            })}
+
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/questionData.js
@@ -0,0 +1,29 @@
+export const questionData = [
+  {
+    id: 1,
+    question: "Here is the question text. What is the answer?",
+    radioName: "question1",
+    answers: [
+      {
+        id: "a1",
+        answer: "Wrong Answer",
+        isTrue: false,
+      },
+      {
+        id: "a2",
+        answer: "Wrong Answer",
+        isTrue: false,
+      },
+      {
+        id: "a3",
+        answer: "Right Answer",
+        isTrue: true,
+      },
+      {
+        id: "a4",
+        answer: "Wrong Answer",
+        isTrue: false,
+      },
+    ],
+  },
+];
